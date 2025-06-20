8e9bd53519ffe890016185232fa352c2168b112f
Robin <rehill012@gmail.com>
2025-06-20 09:13:16 -0400

    Fiksis aspekton de navigaciejo dum rulumado

--- PoshReVo2/ChefaNavigationController.swift
+++ PoshReVo2/ChefaNavigationController.swift
@@ -3,6 +3,12 @@ import UIKit
 final class ChefaNavigationController: UINavigationController {
 	override func viewDidLoad() {
 		navigationBar.isTranslucent = false
-		navigationBar.backgroundColor = DinamikaStilo.koloraFono
+		
+		let navigacejAspekto = UINavigationBarAppearance()
+		navigacejAspekto.configureWithOpaqueBackground()
+		navigacejAspekto.backgroundColor = DinamikaStilo.koloraFono
+		UINavigationBar.appearance().standardAppearance = navigacejAspekto
+		UINavigationBar.appearance().compactAppearance = navigacejAspekto
+		UINavigationBar.appearance().scrollEdgeAppearance = navigacejAspekto
 	}
 }
