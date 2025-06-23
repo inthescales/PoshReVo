94523793eb0e8f26f949db5f8b51daaca302de54
Robin <rehill012@gmail.com>
2025-06-22 21:28:12 -0400

    Artikolotabelo ekaperas en ghusta supra loko

--- PoshReVo2/Artikoloj/ArtikoloViewController.swift
+++ PoshReVo2/Artikoloj/ArtikoloViewController.swift
@@ -187,6 +187,11 @@ final class ArtikoloViewController: UIViewController {
 		)
 	}
 	
+	override func viewWillAppear(_ animated: Bool) {
+		super.viewWillAppear(animated)	
+		tabelo.scrollToRow(at: IndexPath(row: 0, section: 0), at: .top, animated: false)
+	}
+	
 	override func viewDidAppear(_ animated: Bool) {
 		super.viewDidAppear(animated)
 		
