e89f7749d699b99d567803f775437bf72860032f
Robin <rehill012@gmail.com>
2025-08-18 13:09:15 -0400

    (intertempe?) forigis punkton post subartikol-numeroj

--- ReVoDatumbazo/DatumbazFarilo/Artikoloj/NodTraktado/TraktiSubartikolon.swift
+++ ReVoDatumbazo/DatumbazFarilo/Artikoloj/NodTraktado/TraktiSubartikolon.swift
@@ -8,7 +8,7 @@ extension ArboAnalizilo {
 		
 		let akumulilo = BlokAkumulilo()
 		akumulilo.aldoni(blokojn: [
-			.dividila(teksto: ArtikolTeksto.romajCiferoj(por: numero + 1) + ".")
+			.dividila(teksto: ArtikolTeksto.romajCiferoj(por: numero + 1))
 		])
 		
 		traktiFilojn(de: subartikolo, stato: stato) { filo in
